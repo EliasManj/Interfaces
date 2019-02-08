1551456e1f050cc3776ce706557d6927b1075ca3
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2019-02-08 15:03:09 -0600

    define a test signal for serial

---
+++ UART_Software/Project_Headers/Test_Signal.h
@@ -0,0 +1,14 @@
+/*
+ * Test_Signal.h
+ *
+ *  Created on: Feb 7, 2019
+ *      Author: Miguel Elias
+ */
+
+#ifndef TEST_SIGNAL_H_
+#define TEST_SIGNAL_H_
+
+void Test_Signal_Init();
+void Test_Signal_Toggle();
+
+#endif /* TEST_SIGNAL_H_ */
