88c5017620ef9eb175d8e674677ccc483469f0d6
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2019-02-08 15:03:39 -0600

    test signal

--- UART_Software/Sources/Test_Signal.c
+++ UART_Software/Sources/Test_Signal.c
@@ -5,4 +5,24 @@
  *      Author: Miguel Elias
  */
 
+#include "Test_Signal.h"
+#include "derivative.h" /* include peripheral declarations */
 
+//Use PTB2
+
+void Test_Signal_Init()
+{
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	
+	PORTB_PCR2 |= (1<<8);
+	GPIOB_PDDR |= (1<<2);
+	
+	PORTB_PCR21 = (1 << 8);		//Set PTB21 as GPIO
+	GPIOB_PDDR |= (1 << 21);	//Set PTB21 as output
+}
+
+void Test_Signal_Toggle()
+{
+	GPIOB_PDOR ^= (1<<2);
+	GPIOB_PDOR ^= (1<<21); //Put PTB21 as HIGH
+}
